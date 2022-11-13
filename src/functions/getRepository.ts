baf13fa6dc0a2f747d10691f4fc5b9231f9b9049
Taha Kaçmaz <hi@noirs.me>
2022-11-13 22:38:21 +0300

    added: node-fetch because of vercel's node version

--- src/functions/getRepository.ts
+++ src/functions/getRepository.ts
@@ -1,13 +1,14 @@
 import { FileResponse } from "./../types/types.d";
-
+import fetch from "node-fetch";
 export async function getRepository(
   username: string,
   repository: string,
   branch: string = "master"
 ): Promise<FileResponse> {
-  return fetch(
-    `https://api.github.com/repos/${username}/${repository}/git/trees/${branch}?recursive=1`
-  )
+   
+  let response:any = fetch(`https://api.github.com/repos/${username}/${repository}/git/trees/${branch}?recursive=1`)
     .then((response) => response.json())
     .then((data) => data);
+
+  return response;
 }
