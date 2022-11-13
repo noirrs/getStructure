fcc7bf1a546f9351e3311429d2faae0639940995
Taha Kaçmaz <hi@noirs.me>
2022-11-13 23:06:41 +0300

    changed: node-fetch to axios

--- src/functions/getRepository.ts
+++ src/functions/getRepository.ts
@@ -1,14 +1,18 @@
 import { FileResponse } from "./../types/types.d";
 import fetch from "node-fetch";
+const axios = require("axios")
+
 export async function getRepository(
   username: string,
   repository: string,
   branch: string = "master"
 ): Promise<FileResponse> {
    
-  let response:any = fetch(`https://api.github.com/repos/${username}/${repository}/git/trees/${branch}?recursive=1`)
-    .then((response) => response.json())
-    .then((data) => data);
 
-  return response;
+  return axios.get(`https://api.github.com/repos/${username}/${repository}/git/trees/${branch}?recursive=1`)
+    .then((data) => {
+      return data.data
+    });
+
+
 }
