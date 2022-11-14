068cec68a2d361ad680016bde1a336476d019235
Taha Kaçmaz <hi@noirs.me>
2022-11-14 17:19:28 +0300

    added: class-validators

--- src/modules/app/dtos/request.dto.ts
+++ src/modules/app/dtos/request.dto.ts
@@ -1,13 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class RequestDTO {
 
   @ApiProperty()
+  @IsString()
   username: string;
 
   @ApiProperty()
+  @IsString()
   repository: string;
 
   @ApiProperty()
+  @IsString()
   branch: string;
 }
